0f73cbbcec1f9eb921351946f0b9221efd27ecb2
Brendan Byrd <GitHub@ResonatorSoft.org>
2018-11-10 17:06:14 +0000

    Add train.sh script
    This script calls FCEUX, which calls the Lua scripts.  This is a lot more
    streamlined than re-compiling FCEUX as a library, using a QT window for the
    display, and all of that neswrap nonsense.

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TOPDIR=$PWD
+PREFIX=$PWD/torch
+
+LD_LIBRARY_PATH=$PREFIX/lib
+export LUA_PATH="./?.lua;./?/init.lua;;"
+
+# Copy stdout and stderr to a logfile
+LOGFILE="logs/dqn_log_`/bin/date +\"%F:%R\"`"
+exec > >(tee -i ${LOGFILE})
+exec 2>&1
+
+$PREFIX/bin/fceux --sound 0 --loadlua $TOPDIR/dqn/train_agent.lua roms/smb.zip
