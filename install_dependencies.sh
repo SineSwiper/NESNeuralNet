8f5a9c76f8d93c8fc2a99c5f4cf61af9a37b0ad8
Brendan Byrd <GitHub@ResonatorSoft.org>
2018-11-10 17:12:57 +0000

    Clean up of install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,8 @@
 # Torch install
 ######################################################################
 
+# Die on any error
+set -e
 
 TOPDIR=$PWD
 
@@ -44,13 +46,11 @@ sudo apt-get update
 
 echo "==> Torch7's dependencies have been installed"
 
-
-
-
+# Create directory for sources
+mkdir -p $TOPDIR/src
 
 # Build and install Torch7
-cd /tmp
-rm -rf luajit-rocks
+cd $TOPDIR/src
 git clone https://github.com/torch/luajit-rocks.git
 cd luajit-rocks
 mkdir -p build
@@ -58,19 +58,8 @@ cd build
 git checkout master; git pull
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 make
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 make install
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
-
-
-path_to_nvcc=$(which nvcc)
-if [ -x "$path_to_nvcc" ]
-then
-    cutorch=ok
-    cunn=ok
-fi
 
 # Install base packages:
 $PREFIX/bin/luarocks install cwrap
@@ -78,10 +67,13 @@ $PREFIX/bin/luarocks install paths
 $PREFIX/bin/luarocks install torch
 $PREFIX/bin/luarocks install nn
 
-[ -n "$cutorch" ] && \
-($PREFIX/bin/luarocks install cutorch)
-[ -n "$cunn" ] && \
-($PREFIX/bin/luarocks install cunn)
+# Install GPU packages:
+path_to_nvcc=$(which nvcc)
+if [ -x "$path_to_nvcc" ]
+then
+    $PREFIX/bin/luarocks install cutorch
+    $PREFIX/bin/luarocks install cunn
+fi
 
 $PREFIX/bin/luarocks install luafilesystem
 $PREFIX/bin/luarocks install penlight
@@ -89,52 +81,28 @@ $PREFIX/bin/luarocks install sys
 $PREFIX/bin/luarocks install xlua
 $PREFIX/bin/luarocks install image
 $PREFIX/bin/luarocks install env
-$PREFIX/bin/luarocks install qtlua
-$PREFIX/bin/luarocks install qttorch
+#$PREFIX/bin/luarocks install qtlua
+#$PREFIX/bin/luarocks install qttorch
+$PREFIX/bin/luarocks install nngraph
 
 echo ""
 echo "=> Torch7 has been installed successfully"
 echo ""
 
-
-echo "Installing nngraph ... "
-$PREFIX/bin/luarocks install nngraph
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
-echo "nngraph installation completed"
-
-echo "Installing FCEUX_Learning_Environment ... "
-cd /tmp
-rm -rf FCEUX_Learning_Environment
-git clone https://github.com/ehrenbrav/FCEUX_Learning_Environment.git
-cd FCEUX_Learning_Environment
-$PREFIX/bin/luarocks make
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
+echo "Installing FCEUX ... "
+cd $TOPDIR/src
+git clone https://github.com/TASVideos/fceux
+cd fceux
+LUA_LINKFLAGS='-L'$PREFIX'/lib -lluajit' LUA_INCDIR=$PREFIX'/include' scons SYSTEM_LUA=1 --prefix $PREFIX install
 echo "FCEUX installation completed"
 
-echo "Installing neswrap ... "
-cd /tmp
-rm -rf neswrap
-git clone https://github.com/ehrenbrav/neswrap.git
-cd neswrap
-$PREFIX/bin/luarocks make
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
-echo "neswrap installation completed"
-
 echo "Installing Lua-GD ... "
-mkdir $PREFIX/src
-cd $PREFIX/src
-rm -rf lua-gd
+cd $TOPDIR/src
 git clone https://github.com/ittner/lua-gd.git
 cd lua-gd
 sed -i "s/LUABIN=lua5.1/LUABIN=..\/..\/bin\/luajit/" Makefile
 $PREFIX/bin/luarocks make
-RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 echo "Lua-GD installation completed"
-
-#echo "Installing GPU dependencies..."
-#$PREFIX/bin/luarocks install cutorch
-#$PREFIX/bin/luarocks install cunn
-#echo "Done trying to install the GPU dependencies."
 
 echo
 echo "All done!"
